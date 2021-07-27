3cfb2a4f93ecc3a36966589b1c3370e481205e2b
PenkinLucky <80094831+PenkinLucky@users.noreply.github.com>
2021-07-27 10:25:04 +0300

    Update java.js

--- Script/Java/java.js
+++ Script/Java/java.js
@@ -1,16 +1,18 @@
 //1.Заполнить массив А случайными числами (диазпазон на ваше усмотрение). Заменить каждый элемент массива с нечетным индексом на ноль.
+
+
 var arr = [];
 arr.length = 10;
-i = 0;
 console.log(arr, 'arrStart')
-console.log(i, 'iStart');
 for(i = 0; i < arr.length; i+=2){
     arr[i] = Math.round(Math.random() * 60);
-    console.log(arr,"arrMidle");
-    console.log(i, 'iMidle');
 }
 for(i= 1; i < arr.length; i+=2){
     arr[i] = 0;
 }
 console.log(arr, 'arrFinish');
-console.log(i, 'iFinish');
+
+//2.Заполнить массив размерностью n значениями с клавиатуры. Если введенное значение пользователя не число, или отрицательное значение, то запросить повторный ввод данных.
+
+
+
